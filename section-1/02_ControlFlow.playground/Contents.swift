170aa37fbf93865fbe5c13fc8495f336c2b35c74
Alpha Lin <ed.alpha@gmail.com>
2017-06-23 19:28:53 +0800

    Experiments of control flow in a swift tour

---
+++ section-1/02_ControlFlow.playground/Contents.swift
@@ -0,0 +1,70 @@
+var optionalString: String? = "Hello"
+optionalString == nil
+
+var optionalName: String? = "John Appleseed"
+var greeting = "Hello!"
+if let name = optionalName {
+    greeting = "Hello, \(name)"
+}
+
+// EXPERIMENT
+// Change optionalName to nil. What greeting do you get? Add an else clause that sets a different greeting if optionalName is nil.
+
+optionalName = nil
+if let name = optionalName {
+    greeting = "Hello, \(name)."
+} else {
+    greeting = "Hello, what's your name?"
+}
+
+let vegetable = "red pepper"
+switch vegetable {
+case "celery":
+    print("Add some raisins and make ants on a log.")
+case "cucumber", "watercress":
+    print("That would make a good tea sandwich.")
+case let x where x.hasSuffix("pepper"):
+    print("Is it a spicy \(x)?")
+default:
+    print("Everything tastes good in soup.")
+}
+
+// EXPERIMENT
+// Try removing the default case. What error do you get?
+
+// Error: switch must be exhaustive, consider adding a default clause
+
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25],
+]
+var largest = 0
+for (kind, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+        }
+    }
+}
+print(largest)
+
+// EXPERIMENT
+// Add another variable to keep track of which kind of number was the largest, as well as what that largest number was.
+
+largest = 0
+var largestKind = ""
+for (kind, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+            largestKind = kind
+        }
+    }
+}
+print(largestKind)
+
+
+
+
+
