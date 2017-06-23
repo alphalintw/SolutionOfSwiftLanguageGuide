27135e1fa6a0ecbb33d6963e799a329bdc098b1a
Alpha Lin <ed.alpha@gmail.com>
2017-06-23 22:11:17 +0800

    Experiments of Error Handling in a swift tour

---
+++ section-1/07_ErrorHandling.playground/Contents.swift
@@ -0,0 +1,37 @@
+enum PrinterError: Error {
+    case outOfPaper
+    case noToner
+    case onFire
+}
+
+func send(job: Int, toPrinter printerName: String) throws -> String {
+    if printerName == "Never Has Toner" {
+        throw PrinterError.noToner
+    }
+    return "Job sent"
+}
+
+// EXPERIMENT
+// Change the printer name to "Never Has Toner", so that the send(job:toPrinter:) function throws an error.
+
+do {
+    let printerResponse = try send(job: 1040, toPrinter: "Never Has Toner")
+    print(printerResponse)
+} catch {
+    print(error)
+}
+
+// EXPERIMENT
+// Add code to throw an error inside the do block. What kind of error do you need to throw so that the error is handled by the first catch block? What about the second and third blocks?
+
+do {
+    throw PrinterError.onFire
+    let printerResponse = try send(job: 1440, toPrinter: "Gutenberg")
+    print(printerResponse)
+} catch PrinterError.onFire {
+    print("I'll just put this over here, with the rest of the fire.")
+} catch let printerError as PrinterError {
+    print("Printer error: \(printerError).")
+} catch {
+    print(error)
+}
